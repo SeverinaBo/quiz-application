ce1c4a068dbd739f123ffa30a5e240ac74ca832e
Severina <severinasiur@gmail.com>
2023-02-15 14:40:59 +0200

    Added SignIn page with Mui template. Will remove login and registration created by me.

--- src/App.js
+++ src/App.js
@@ -3,22 +3,24 @@ import LogIn from "./RegistrationAndLogin/LogIn";
 import Register from "./RegistrationAndLogin/Register";
 
 import AppStyle from "./style/AppStyle.css";
+import SignIn from "./SignInAndSignUpMui/SignIn";
 
 
 function App() {
-    const [currentForm,setCurrentForm] = useState('login');
+  /*  const [currentForm,setCurrentForm] = useState('login');
 
     const toggleForm = (formName) =>{
         setCurrentForm(formName);
-    }
+    }*/
 
   return (
-        <div className="App">
+      <SignIn />)
+/*        <div className="App">
             {
                 currentForm === "login" ? <LogIn  onFormSwitch={toggleForm}/> : <Register onFormSwitch={toggleForm} /> //if condition is true it will return first statement if it is false it will return second
             }
         </div>
-  );
+  );*/
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import  {faEye, faEyeSlash }from '@fortawesome/free-solid-svg-icons';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
